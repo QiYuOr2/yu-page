3a9171156da4a1006c2e2a1924250bbb6f3fb24c
qiyuor2 <tuzilow@gmail.com>
2022-01-20 16:03:53 +0800

    chore(proxy): 优化

--- scripts/server.js
+++ scripts/server.js
@@ -3,49 +3,20 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = Express();
 
+const createVueProxy = (target, host) => {
+  app.use(createProxyMiddleware(`/${target}`, { target: host }));
+  app.use(createProxyMiddleware(`/${target}/img`, { target: `${host}/img` }));
+  app.use(createProxyMiddleware(`/${target}/css`, { target: `${host}/css` }));
+  app.use(createProxyMiddleware(`/${target}/js`, { target: `${host}/js` }));
+};
+
 // @yu-page/editor
-app.use(
-  createProxyMiddleware('/editor', {
-    target: 'http://localhost:63642',
-  })
-);
-app.use(
-  createProxyMiddleware('/editor/img', {
-    target: 'http://localhost:63642/img',
-  })
-);
-app.use(
-  createProxyMiddleware('/editor/css', {
-    target: 'http://localhost:63642/css',
-  })
-);
-app.use(
-  createProxyMiddleware('/editor/js', {
-    target: 'http://localhost:63642/js',
-  })
-);
+const EDITOR_HOST = 'http://localhost:63642';
+createVueProxy('editor', EDITOR_HOST);
 
 // @yu-page/template
-app.use(
-  createProxyMiddleware('/template', {
-    target: 'http://localhost:63641',
-  })
-);
-app.use(
-  createProxyMiddleware('/template/img', {
-    target: 'http://localhost:63641/img',
-  })
-);
-app.use(
-  createProxyMiddleware('/template/css', {
-    target: 'http://localhost:63641/css',
-  })
-);
-app.use(
-  createProxyMiddleware('/template/js', {
-    target: 'http://localhost:63641/js',
-  })
-);
+const TEMPLATE_HOST = 'http://localhost:63641';
+createVueProxy('template', TEMPLATE_HOST);
 
 app.listen(3090, () => {
   console.log('http://localhost:3090');
