51e21c5ae27fa92c27251f0757d5da97e405e003
qiyuor2 <tuzilow@gmail.com>
2022-03-17 16:15:02 +0800

    fix(editor): base url prod

--- packages/editor/src/common/config.ts
+++ packages/editor/src/common/config.ts
@@ -4,11 +4,11 @@ const _config: Record<string, Record<string, string>> = {
     IFRAME_HOST: 'http://localhost',
   },
   production: {
-    BASE_URL: 'http://localhost:3090/api',
+    BASE_URL: 'http://101.43.4.73:3090/api',
     IFRAME_HOST: 'http://101.43.4.73',
   },
   prod: {
-    BASE_URL: 'http://localhost:3090/api',
+    BASE_URL: 'http://101.43.4.73:3090/api',
     IFRAME_HOST: 'http://101.43.4.73',
   },
 };
